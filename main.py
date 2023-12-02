c9a0b23e148781c192641a26794910355dfc910e
Elvis-785 <kiruielvis3358@gmail.com>
2023-12-02 21:02:30 +0300

    changed the structure

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
-from task_db import db, Qtask
-#crud
+from tinydb import TinyDB, Query
+db = TinyDB('todo.json')
 
-def task_manager():
-    choice=0
-    while choice != 5:
-        print("""
-Welcome to your task manager, what would you like to do?
-1.Add a task.
-2.View your task.
-3.Update your task.
+def addTask():
+    print('Adding task>>>')
+    title = input('Enter task title: ')
+    descr = input('Enter task description: ')
+    start_time = input('Enter starting time: ')
+    end_time = input('Enter task ending time: ')
+    completion = False
+    task = db.insert({'title': title,
+                      'description':descr,
+                      'starting time':start_time,
+                      'Ending time':end_time,
+                      'Completion':completion,
+                      })
+        
+def viewTask():
+    print('View your tasks...')
+    for task in db:
+        print(task)
+    
+def updateTask():
+    key = input('New task update: ')
+    value = input('New value update: ')
+    id = int(input('Enter task id: '))
+    return db.update({key:value}, doc_ids=[id])
+
+def deleteTask():
+    return db.truncate()
+
+while True:
+    user_input = input("""
+What would you like to do?
+1.Add a new task.
+2.Look up task.
+3.Update the task.
 4.Delete task.
-5.Quit.
-        """)
-        choice=int(input())
-        if choice==1:
-            print("Adding tasks >>>")
-            title=input("Enter the title: ")
-            descr=input("Enter task description: ")
-            start_time=input("Enter the starting time: ")
-            end_time=input("Enter the ending time: ")
-            Completion=False
-            task= db.insert({"title":title,
-                        "description":descr, 
-                        "starting time":start_time, 
-                        "Ending time": end_time, 
-                        "Completion":Completion})
-
-        elif choice==2:
-            print("View your tasks...")
-            for task in db:
-                print(task)
-        elif choice==3:
-            key=input("New task update: ")
-            value=input("New value update: ")
-            id=int(input("Enter task id: "))
-            db.update({key:value}, doc_ids=[id])
-        elif choice==4:
-            
-            db.truncate()
-            
-
-
-
-
-    print("Exit program")
-
-if __name__=="__main__":
-    task_manager()
+5.Quit.\n""")
+    if user_input == '1':
+        addTask()
+    if user_input == '2':
+        viewTask()
+    if user_input == '3':
+        updateTask()
+    if user_input == '4':
+        deleteTask()
+    if user_input == ['q', 'Q']:
+        print('Exiting')
+        break
         
--- task_db.py
+++
@@ -1,4 +0,0 @@
-from tinydb import TinyDB, Query
-db = TinyDB("todo.json")
-db.default_table_name="tasks"
-Qtask=Query()
